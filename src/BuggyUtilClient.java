09b37e7c216b2537abb33e26a482c02b567d04c5
Clark Chan <chan239@purdue.edu>
2018-10-06 15:37:53 -0400

    this is a commitment

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -1,7 +1,7 @@
 
 public class BuggyUtilClient {
     public static void main(String[] args) {
-        //this is a test to see if i can bust nuts  boi
+        //this is a test to see if i can bust nuts
         // First test case in Main
         System.out.println("Example 1: Make My Username");
         System.out.println("In: first = " + "\"Sean\"" +
